5b6f9161e8b954d3775e9aaa5a646e63cd3ad2f6
Eric Normann <eric.n@me.com>
2021-02-15 21:05:04 -0500

    add call by id

--- server.js
+++ server.js
@@ -54,3 +54,17 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 })
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+    
+})
